eb8fbd1c3190cdea09ce05b8cf1eb637d95c3d74
liliamezghani <l.mezghani.lm@gmail.com>
2025-10-25 23:37:09 +0200

    submit of tp3-2

--- src/Operation.java
+++ src/Operation.java
@@ -1,3 +1,11 @@
-public class Operation {
-
+import java.io.Serializable;
+public class Operation implements Serializable {
+    public double n1;
+    public double n2;
+    public String op;
+    public Operation(double n1, double n2, String op) {
+        this.n1 = n1;
+        this.n2 = n2;
+        this.op = op;
+    }
 }
